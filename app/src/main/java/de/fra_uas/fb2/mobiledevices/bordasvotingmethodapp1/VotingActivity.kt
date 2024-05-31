7d66ef40f243e29be55ec797fbaab9c410cc001f
Rajpreet0 <Rajpreet.Singh@gmx.de>
2024-05-31 13:47:46 +0200

    refactored intent to startActivityOnResult

--- app/src/main/java/de/fra_uas/fb2/mobiledevices/bordasvotingmethodapp1/VotingActivity.kt
+++ app/src/main/java/de/fra_uas/fb2/mobiledevices/bordasvotingmethodapp1/VotingActivity.kt
@@ -2,6 +2,7 @@ package de.fra_uas.fb2.mobiledevices.bordasvotingmethodapp1
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,15 +15,21 @@ class VotingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_voting)
 
     }
+    private fun shareData(){
+        val result = Intent()
+        val bundle = intent.extras
+
+        result.putExtra("updatedVoteCount", bundle!!.getInt("voteCount"))               // Pass the Votes Count back to the User
+        result.putExtra("numOptionsUpdated", bundle.getString("numOptions"))            // Pass numOptions for persistent data
+        result.putExtra("votingOptionsUpdated", bundle.getString("votingOptions"))      // Pass votingOptions for persistent data
+        setResult(RESULT_OK, result)
+        Log.d("Data:", result.toString())
+        finish()
+    }
 
     // Function to add a single vote
     fun confirmBtn(view: View) {
-        val intent: Intent = Intent(view.context, MainActivity::class.java).apply {
-            putExtra("updatedVoteCount",  intent.getIntExtra("voteCount", 0))       // Pass the Votes Count back to the User
-            putExtra("numOptionsUpdated", intent.getStringExtra("numOptions"))                 // Pass numOptions for persistent data
-            putExtra("votingOptionsUpdated", intent.getStringExtra("votingOptions"))           // Pass votingOptions for persistent data
-        }
-        view.context.startActivity(intent)
+        shareData();
 
     }
 
